7a181bb33cf033eb96adb938f5787a630748cbc9
adhithin <adhithi.nmurthy07@gmail.com>
2021-03-11 17:09:38 -0800

    ooh we can send emails on this now

--- bananascraper.py
+++ bananascraper.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
-URL = 'https://en.wikipedia.org/wiki/Banana'
+bananaURL = 'https://en.wikipedia.org/wiki/Banana'
 
 headers = { "User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 
-page = requests.get(URL, headers=headers)
+page = requests.get(bananaURL, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -13,4 +14,45 @@ title = soup.find(id="firstHeading").get_text()
 
 print(title)
 
-print(URL)
+print(bananaURL)
+
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('adhithi.nmurthy07@gmail.com', 'yavesbrwlogwvuaa')
+
+    subject = 'Bananas'
+
+    body = 'Check the website to learn about Bananas' \
+           'https://en.wikipedia.org/wiki/Banana'
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        'adhithi.nmurthy07@gmail.com',
+        'adhithi.nmurthy07@gmail.com',
+        'msg'
+    )
+
+    print("email has been sent!")
+
+    server.quit()
+
+answer = input("do you want this sent to your email?")
+
+if answer == "yes":
+    send_email()
+else:
+    print("okay, email will not send")
+
+
+
+
+
+
+
+
+
